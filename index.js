e30f223332fa8fc5e82590146583a645d97b8137
Guillermo <Guillermolopezargudo@gmail.com>
2024-04-10 10:51:13 +0200

    Integracion BBDD a Backend

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-
+const Note = require('./models/note')
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -29,18 +30,15 @@ let notes = [
   })
   
   app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })  
   })
 
   app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    
-    if (note) {
+    Note.findById(request.params.id).then(note => {
       response.json(note)
-    } else {
-      response.status(404).end()
-    }
+    })
   })
 
   app.delete('/api/notes/:id', (request, response) => {
@@ -66,18 +64,17 @@ let notes = [
       })
     }
   
-    const note = {
+    const note = new Note({
       content: body.content,
-      important: Boolean(body.important) || false,
-      id: generateId(),
-    }
+      important: body.important || false,
+    })
   
-    notes = notes.concat(note)
-  
-    response.json(note)
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
   })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
